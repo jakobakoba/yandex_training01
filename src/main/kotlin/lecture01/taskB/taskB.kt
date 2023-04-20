13640a9dfefb3924bafb01514313cb3ac1fc3c33
jakobakoba <jakobakoba@gmail.com>
2023-04-20 15:36:41 +0600

    lecture01 taskB

---
+++ src/main/kotlin/lecture01/taskB/taskB.kt
@@ -0,0 +1,66 @@
+import java.io.PrintWriter
+import java.util.StringTokenizer
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+//private val INPUT = File("input.txt").inputStream()
+//private val OUTPUT = File("output.txt").outputStream()
+private val INPUT = System.`in`
+private val OUTPUT = System.out
+
+private val bufferedReader = INPUT.bufferedReader()
+private val outputWriter = PrintWriter(OUTPUT, false)
+private fun readLn() = bufferedReader.readLine()!!
+
+private fun readList() = readLn().split(' ')
+private var tokenizer = StringTokenizer("")
+private fun read(): String {
+    while (tokenizer.hasMoreTokens().not()) tokenizer = StringTokenizer(readLn(), " ")
+    return tokenizer.nextToken()
+}
+
+private fun readInt() = read().toInt()
+private fun readLong() = read().toLong()
+private fun readDouble() = read().toDouble()
+
+private fun readIntList() = readList().map { it.toInt() }
+private fun readLongList() = readList().map { it.toLong() }
+private fun readDoubleList() = readList().map { it.toDouble() }
+
+private fun readIntArray(n: Int = 0) =
+    if (n == 0) readList().run { IntArray(size) { get(it).toInt() } } else IntArray(n) { readInt() }
+
+private fun readLongArray(n: Int = 0) =
+    if (n == 0) readList().run { LongArray(size) { get(it).toLong() } } else LongArray(n) { readLong() }
+
+private fun readDoubleArray(n: Int = 0) =
+    if (n == 0) readList().run { DoubleArray(size) { get(it).toDouble() } } else DoubleArray(n) { readDouble() }
+
+
+private fun Int.modPositive(other: Int): Int = if (this % other < 0) ((this % other) + other) else (this % other)
+
+
+private class taskB {
+    fun solveTestCase(): String {
+        //TODO: Solve the question
+        val a = readInt()
+        val b = readInt()
+        val c = readInt()
+
+        val result = if (a + b > c && a + c > b && c + b > a) "YES" else "NO"
+        return result
+    }
+}
+
+fun main(args: Array<String>) {
+
+    //TODO: Read in each Test Case
+
+    outputWriter.println(
+        taskB()
+            .solveTestCase()
+    )
+
+
+    outputWriter.flush()
+}
